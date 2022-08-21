97d46a7aceb3f9b18b23104f9dcf5a5dbfdd77ff
rfc <rfcfct@gmail.com>
2022-08-21 02:40:34 +0200

    Test 1

---
+++ crptapibackend/src/main/java/com/crptapi/crptapibackend/CrptapibackendApplication.java
@@ -0,0 +1,13 @@
+package com.crptapi.crptapibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrptapibackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrptapibackendApplication.class, args);
+	}
+
+}
---
+++ crptapibackend/src/test/java/com/crptapi/crptapibackend/CrptapibackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crptapi.crptapibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrptapibackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
